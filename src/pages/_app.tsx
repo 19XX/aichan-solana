be90c9caa9c4092c3ca28efb146acd3b53db3aec
19XX <93529730+19XX@users.noreply.github.com>
2025-02-06 04:33:48 +0900

    Fix ESLint issue: Unused Montserrat & inter in _app.tsx

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,18 +1,18 @@
 import "../styles/globals.css";
 import type { AppProps } from "next/app";
-import { WalletProviderComponent } from "../components/WalletProvider";
+import WalletProviderComponent from "../components/WalletProvider";
 import { Montserrat, Inter } from "next/font/google";
 
-const inter = Inter({ subsets: ["latin"] });
 const montserrat = Montserrat({ subsets: ["latin"] });
+const inter = Inter({ subsets: ["latin"] });
 
 function MyApp({ Component, pageProps }: AppProps) {
   return (
-    <div className={`${inter.className} ${montserrat.className}`}>
-      <WalletProviderComponent>
+    <WalletProviderComponent>
+      <main className={`${montserrat.className} ${inter.className}`}>
         <Component {...pageProps} />
-      </WalletProviderComponent>
-    </div>
+      </main>
+    </WalletProviderComponent>
   );
 }
 
